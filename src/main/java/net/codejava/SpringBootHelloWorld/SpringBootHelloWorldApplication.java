1bc28c7492dafc55bd76ad88083ace8aa3ca45bf
lalaro <lauraortegon7@gmail.com>
2024-02-29 18:54:25 -0500

    Funciona parte1

--- src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
+++ src/main/java/net/codejava/SpringBootHelloWorld/SpringBootHelloWorldApplication.java
@@ -2,12 +2,25 @@ package net.codejava.SpringBootHelloWorld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class SpringBootHelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloWorldApplication.class, args);
+    }
 
+    @RequestMapping("/")
+    public String home() {
+        return "<h1>Hello world Spring Boot</h1><p>Welcome to my Spring Boot application!</p>";
+    }
+
+    @RequestMapping("/new")
+    public String newEndpoint() {
+        return "<h2>This is a new endpoint</h2><p>Welcome to the new endpoint!</p>";
+    }
 }
